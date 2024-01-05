c3f4bcb25c4eefddcf768f649049cddb5a45f6b8
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-04 20:44:29 -0500

    create get route

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/get-user<user_id>")
+def get_user(user_id):
+  user_data = {
+    'user_id': user_id,
+    'name': "Huck Finn",
+    'email': 'huck.finn@example.com'
+  }
+
+  extra = request.args.get('extra')
+  if extra:
+    user_data['extra'] = extra
+
+  return jsonify(user_data), 200
+
+'''@app.route('/')
 def home():
-  return "Home"
+  return "Home" '''
 
 if __name__ == "__main__":
   app.run(debug=True)
